4806f59f7524127437920f0fadceef54ec05904b
Cody Brinkman <codybrinkmansoccer@gmail.com>
2019-10-09 10:39:21 -0400

    add front end production build

---
+++ build/precache-manifest.2c798606625428e25dca5365a1078c6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3219f79c8c07172dcd5918e688a666",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e89deaa3dac8002849a",
+    "url": "/static/css/main.6a943033.chunk.css"
+  },
+  {
+    "revision": "9d7d7637ffdb3543cf9b",
+    "url": "/static/js/2.bf711040.chunk.js"
+  },
+  {
+    "revision": "2e89deaa3dac8002849a",
+    "url": "/static/js/main.b81f2b69.chunk.js"
+  },
+  {
+    "revision": "17d0ae5722a7a1d90858",
+    "url": "/static/js/runtime-main.86de38e2.js"
+  }
+]);
